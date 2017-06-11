2dfd673954295513fd6c4e4bb3fd217efdd1ab83
Nabeel Sarwar <nabeelsarwar200@gmail.com>
2017-06-11 01:56:01 -0400

    removed match_id and added gradiant clipping so that the first layer doesnot just become the same value for everything. match_id was driving the weights to make tanh always output -1 or 1 because of high values for weights

--- AnalysisAWS.py
+++ AnalysisAWS.py
@@ -77,7 +77,7 @@ df_catInteger_features_example = filter(lambda x: 'hero_id' in x, columns)
 
 from itertools import chain
 # these will require string processing on the column names to work
-numericalFeatures = ['match_id', 'positive_votes', 'negative_votes', 'first_blood_time', 'radiant_win',
+numericalFeatures = ['positive_votes', 'negative_votes', 'first_blood_time', 'radiant_win',
                     'duration', 'kills', 'deaths', 'assists', 'kpm', 'kda', 'hero_dmg',
                     'gpm', 'hero_heal', 'xpm', 'totalgold', 'totalxp', 'lasthits', 'denies',
                     'tower_kills', 'courier_kills', 'observer_uses', 'sentry_uses',
@@ -95,6 +95,7 @@ catFull = list(chain(*catFull))
 
 # In[ ]:
 
+match_ids = df['match_id']
 df_numerical = df[numFeatures]
 df_numerical.loc[:, 'radiant_win'] = df_numerical.loc[:, 'radiant_win'].apply(lambda x : int(x))
 df_numerical.iloc[:, 1:len(df_numerical.columns)] = df_numerical.iloc[:, 1:len(df_numerical.columns)].apply(lambda x: (x - np.nanmean(x)) / (np.nanmax(x) - np.nanmin(x)))
@@ -108,17 +109,16 @@ df = df_numerical
 x = np.random.rand(df.shape[0])
 mask = np.where(x < 0.75)[0]
 mask2 = np.where(x >= 0.75)[0]
-
-
-# In[ ]:
-
-df_train, df_test = np.split(df, [int(.75*len(df))])
+df_train = df.iloc[mask, :]
+df_test = df.iloc[mask2, :]
+match_ids_train = match_ids.iloc[mask]
+match_ids_test = match_ids.iloc[mask2]
 
 
 # In[ ]:
 
 NumFeatures = df.shape[1]
-layer_size = [int(NumFeatures/2.0), NumFeatures]
+layer_size = [int(NumFeatures * 0.75), NumFeatures]
 
 
 # In[ ]:
@@ -136,23 +136,26 @@ print df_train.shape
 x = tf.placeholder(tf.float32, [None, NumFeatures])
 y = x
 #encoders
-harmonic_mean1 = np.sqrt(1.0 * (NumFeatures + layer_size[0]) / (NumFeatures * layer_size[0]))
-weights_1 = tf.Variable(tf.random_normal([NumFeatures, layer_size[0]], stddev=harmonic_mean1), name='weights_1')
-bias_1 = tf.Variable(tf.random_normal([layer_size[0]], stddev = harmonic_mean1), name='bias_1')
+weights_1 = tf.Variable(tf.random_normal([NumFeatures, layer_size[0]], stddev = 1.0/NumFeatures/100), name='weights_1')
+bias_1 = tf.Variable(tf.random_normal([layer_size[0]], stddev = 1.0/NumFeatures), name='bias_1')
 
-    
 #decoders
-harmonic_mean2 = np.sqrt(1.0 * (layer_size[1] + layer_size[0]) / (layer_size[1] * layer_size[0]))
-weights_2 = tf.Variable(tf.random_normal([layer_size[0], layer_size[1]], stddev = harmonic_mean2), name='weights_2')
-bias_2 = tf.Variable(tf.random_normal([layer_size[1]], stddev = harmonic_mean2), name='bias_2')
+weights_2 = tf.Variable(tf.random_normal([layer_size[0], layer_size[1]], stddev = 1.0/NumFeatures/100), name='weights_2')
+bias_2 = tf.Variable(tf.random_normal([layer_size[1]], stddev = 1.0/NumFeatures), name='bias_2')
   
 layer1 = tf.tanh(tf.matmul(x, weights_1))
 output = tf.tanh(tf.matmul(layer1, weights_2))
-    
-cost = tf.reduce_mean(tf.reduce_sum(tf.pow(y[:, 1:y.shape[1].value]-output[:, 1:y.shape[1].value], 2), 1))
+
+cost = tf.reduce_mean(tf.reduce_sum(tf.pow(y-output, 2), 1))
 rank = tf.rank(cost)
 
-optimizer = tf.train.AdamOptimizer().minimize(cost)
+learning_rate = 0.000001
+beta1 = 0.5
+beta2 = 0.5
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate, beta1=beta1, beta2=beta2)
+gradients, variables = zip(*optimizer.compute_gradients(cost))
+gradients, _ = tf.clip_by_global_norm(gradients, 5.0)
+train_op = optimizer.apply_gradients(zip(gradients, variables))
     
 variable_dict = {'weights_1': weights_1, 'weights_2': weights_2,
                      'bias_1': bias_1, 'bias_2': bias_2}
@@ -180,7 +183,7 @@ def test(sess, test_data):
     data = data.astype(np.float32)
     layer1 = tf.tanh(tf.matmul(data, weights_1))
     output = tf.tanh(tf.matmul(layer1, weights_2))
-    residuals = tf.reduce_sum(tf.abs(output[:, 1:output.shape[1].value] - tf.cast(data[:, 1:output.shape[1].value], tf.float32)), axis = 1)
+    residuals = tf.reduce_sum(tf.abs(output - tf.cast(data, tf.float32)), axis = 1)
     output_results, residuals = sess.run([output, residuals])
     indices = np.argsort(residuals)[::-1]
     return data, output_results, indices, residuals
@@ -199,7 +202,7 @@ def train():
             saver.save(sess, ckpoint_dir)
         for batchItr in xrange(numBatches):
             batch = df_train.sample(n=batchSize).as_matrix()
-            sess.run(optimizer, feed_dict = {x : batch})
+            sess.run(train_op, feed_dict = {x : batch})
 
 with tf.Session() as sess:
     if sess.run(rank) != 0:
@@ -222,7 +225,7 @@ with tf.Session() as sess:
         np.savetxt("data/anomalies.csv", anomaliesSave, delimiter=",")
         np.savetxt("data/output.csv", output, delimiter=",")
         np.savetxt('data/indices.csv', indices_test, delimiter = ',')
-        anomalizedAnalizable = list(set(anomalies[:, 0]))
+        anomalizedAnalizable = match_ids_test.values
         goodMatches = []
         print len(anomalizedAnalizable)
         for i in range(len(anomalizedAnalizable)):
@@ -236,4 +239,9 @@ with tf.Session() as sess:
 
 print 'Done'
 print datetime.datetime.now()
+
+
+# In[ ]:
+
+
 
